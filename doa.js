4a220e4101f22e1fee17b4098077dfbb311afe84
Brilliahib <brilliahib21@gmail.com>
2024-01-05 20:47:33 +0700

    fix card doa

--- doa.js
+++
@@ -1,5 +0,0 @@
-const endpoint = 'https://doa-doa-api-ahmadramadhan.fly.dev/api'
-
-fetch(endpoint)
-    .then((response) => response.json())
-    .then((data) => console.log(data))
--- jadwal-sholat.js
+++
@@ -1,7 +0,0 @@
-const endpoint = 'https://api.myquran.com/v2/sholat/jadwal/1433/2024/01/03';
-fetch(endpoint)
-    .then((response) => response.json())
-    .then((data) => {
-        const jadwal = data.jadwal
-        console.log(jadwal);
-    });
